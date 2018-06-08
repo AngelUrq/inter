0da50b036e5abf7013e98d1b0845069e50c5d061
AngelUrq <angel4441916@gmail.com>
2018-06-08 00:12:14 -0400

    Contacto agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PortadaComponent } from './portada/portada.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PortadaComponent
+    PortadaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
