e14e06716b96301556b53dd8278d926a5a056f58
AngelUrq <angel4441916@gmail.com>
2018-06-08 00:19:36 -0400

    Page scroll agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PortadaComponent } from './portada/portada.component';
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
 import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
@@ -14,9 +14,11 @@ import { ContactoComponent } from './contacto/contacto.component';
     ContactoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2PageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
